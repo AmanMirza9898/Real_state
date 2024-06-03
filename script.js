4e6351ca3ddba8008a0708e41b9f2ee469e08e5b
Aman Mirza <155296756+AmanMirza9898@users.noreply.github.com>
2024-06-03 22:26:34 +0530

    All things

---
+++ script.js
@@ -0,0 +1,21 @@
+
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    autoplay:{
+      delay: 1000,
+      disableOnInteraction: false,
+  },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+
+  
+
+
+
+
+  
